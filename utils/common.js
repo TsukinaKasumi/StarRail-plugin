8aaa54262a73551428024b9b2128d408fe69af71
ikechan8370 <geyinchi@buaa.edu.cn>
2023-05-02 20:27:53 +0800

    feat: 更新功能

---
+++ utils/common.js
@@ -0,0 +1,16 @@
+import { exec } from 'child_process'
+
+export async function checkPnpm () {
+  let npm = 'npm'
+  let ret = await execSync('pnpm -v')
+  if (ret.stdout) npm = 'pnpm'
+  return npm
+}
+
+async function execSync (cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, { windowsHide: true }, (error, stdout, stderr) => {
+      resolve({ error, stdout, stderr })
+    })
+  })
+}
