9ee1174d37e2d55651bb8a73f6c908972deafec2
loneyclown <40462995@qq.com>
2023-05-22 18:27:29 +0800

    fix bug #1

--- common/runtimeRender.js
+++ common/runtimeRender.js
@@ -1,13 +1,13 @@
 import setting from '../utils/setting.js'
 
-export default function runtimeRender (e, path, data = {}, cfg = {}) {
+export default function runtimeRender (e, path, renderData = {}, cfg = {}) {
   if (!e.runtime) {
     console.log('未找到e.runtime，请升级至最新版Yunzai')
   }
   let scale = setting.getConfig('gachaHelp').renderScale || 100
   scale = Math.min(2, Math.max(0.5, scale / 100))
   const pct = `style=transform:scale(${scale})`
-  return e.runtime.render('StarRail-plugin', path, data, {
+  return e.runtime.render('StarRail-plugin', path, renderData, {
     ...cfg,
     beforeRender ({ data }) {
       return {
