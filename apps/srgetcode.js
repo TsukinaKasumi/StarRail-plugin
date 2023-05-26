f3fda3d0dbc8b1e7924243bb64e19984d2c79f70
huahua <11535210+Sangonomiy@user.noreply.gitee.com>
2023-05-26 17:14:37 +0000

    修复兑换码处理完成不显示的bug
    Signed-off-by: huahua <11535210+Sangonomiy@user.noreply.gitee.com>

--- apps/srgetcode.js
+++ apps/srgetcode.js
@@ -5,72 +5,76 @@ import fetch from 'node-fetch'
 import lodash from 'lodash'
 
 export class exchange extends plugin {
-  constructor(e) {
-    super({
-      name: '星铁plugin兑换码',
-      dsc: '星穹铁道直播兑换码',
-      event: 'message',
-      priority: -1000,
-      rule: [
-        {
-          reg: `^${rulePrefix}兑换码$`,
-          fnc: 'getCode'
-        }
-      ]
-    })
-  }
+ constructor(e) {
+ super({
+ name: '星铁plugin兑换码',
+  dsc: '星穹铁道直播兑换码',
+   event: 'message',
+   priority: -100000,
+   rule: [
+   {
+   reg: /^(#|\*)?(星铁|崩铁)?(直播|前瞻)?兑换码$/,
+   fnc: 'getCode'
+   }
+  ]
+  }  )
+   }
 
   async getCode() {
+    let reg = this.e.msg.match(/^(#|\*)?(星铁|崩铁)?(直播|前瞻)?兑换码$/)
+    let game = 'srActId'
+    if (reg[1] == '*' || ["星铁", "崩铁"].includes(reg[2])) {
+    game = 'srActId'
+    }
     this.code_ver = ''
     this.now = parseInt(Date.now() / 1000)
-    let actid = await this.getActId()
+    let actid = await this.getActId(game)
     if (!actid) return
     this.actId = actid
 
-    /** index info */
-    let index = await this.getData('index')
-    if (!index || !index.data) return
-    if (index.data === null) {
-      return await this.reply(`错误：\n${index.message}`)
-    }
-    
+ /** index info */
+   let index = await this.getData('index')
+   if (!index || !index.data) return
+   if (index.data === null) {
+   return await this.reply(`错误：\n${index.message}`)
+   }
+     
     let index_data = index.data.live;
     let title = index_data['title'];
     this.code_ver = index_data['code_ver'];
     if (index_data.remain > 0) {
-      return await this.reply(`暂无直播兑换码\n${title}`)
-    }
+    return await this.reply(`暂无直播兑换码\n${title}`)
+   }
 
     let code = await this.getData('code')
     if (!code || !code.data?.code_list) return
     let codes = [];
 
-    for (let val of code.data.code_list) {
-      if (val.code){
-        //let title = (val.title || '').replace(/\<.*?\>/g,'')
-        codes.push(val.code)
-      }
-    }
+ for (let val of code.data.code_list) {
+ if (val.code){
+    codes.push(val.code)
+  }
+ }
 
-    let msg = ''
-    if (codes.length >= 3) {
-      msg = [`${title}-直播兑换码`, `兑换码存在有效期，请及时兑换哦~`, ...codes]
-      msg = await common.makeForwardMsg(this.e, msg, msg[0])
+   let msg = ''
+   if (codes.length >= 3) {
+    msg = [`${title}-直播兑换码`, `兑换码存在有效期，请及时兑换哦~`, ...codes]
+    msg = await common.makeForwardMsg(this.e, msg, msg[0])
     } else if (this.e.msg.includes('#')) {
-      msg += codes.join('\n')
+    msg += codes.join('\n')
     } else {
-      msg = `${title}-直播兑换码\n`
-      msg += codes.join('\n')
+    msg = `${title}-直播兑换码\n`
+    msg += codes.join('\n')
     }
 
-    await this.reply(msg)
+   await this.reply(msg)
   }
 
   async getData(type) {
     let url = {
       index: `https://api-takumi.mihoyo.com/event/miyolive/index`,
       code: `https://api-takumi-static.mihoyo.com/event/miyolive/refreshCode?version=${this.code_ver}&time=${this.now}`,
-      actId: "https://bbs-api.mihoyo.com/painter/api/user_instant/list?offset=0&size=20&uid=288909600",
+      srActId: "https://bbs-api.mihoyo.com/painter/api/user_instant/list?offset=0&size=20&uid=288909600",
     }
 
     let response
@@ -94,33 +98,25 @@ export class exchange extends plugin {
     return res
   }
 
-  async getActId() {
+  async getActId(game) {
     // 获取 "act_id"
-    let ret = await this.getData('actId')
+    let ret = await this.getData(game)
     if (ret.error || ret.retcode !== 0) {
       return "";
     }
 
-    let actId = "ea202305250827517022";
-    let keywords = ["前瞻", "前瞻特别节目", "特别节目"];
+    let actId = "";
     for (const p of ret.data.list) {
       const post = p.post.post;
       if (!post) {
         continue;
       }
-      if (!keywords.every((word) => post.subject.includes(word))) {
-        continue;
-      }
-      let shit = JSON.parse(post.structured_content);
-      for (let segment of shit) {
-        if (segment.insert.toString().includes('观看直播') && segment.attributes.link) {
-          let matched = segment.attributes.link.match(/act_id=(.*?)&/);
-          if (matched) {
-            actId = matched[1];
-          }
-        }
-      }
+    let structured_content = post.structured_content
 
+    let result = structured_content.match(/{\"link\":\"https:\/\/webstatic.mihoyo.com\/bbs\/event\/live\/index.html\?act_id=(.*?)\\/)
+    if (result) {
+        actId = result[1]
+    }
       if (actId) {
         break;
       }
