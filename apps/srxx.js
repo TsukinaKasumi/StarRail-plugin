853206facd9d96b569b03fefa07aac2a140b3663
Sangonomiy <860563585@qq.com>
2023-06-30 03:06:11 +0800

    update：更新一下1.1下半版本全角色攻略

--- apps/srxx.js
+++ apps/srxx.js
@@ -50,7 +50,11 @@ export class srxx extends plugin {
         {
           reg: `^${rulePrefix}商店光锥推荐$`,
           fnc: 'srgz'
-        }
+        }     
+        // {
+        //   reg: `^${rulePrefix}(.*)配队$`,
+        //   fnc: 'srpd'
+        // }
       ],
     });
   }
@@ -354,12 +358,13 @@ export class srxx extends plugin {
     return false
  }
 
- async srgz(e) {
+ async srgz (e) {
   let msg = [
       segment.image(`${srrolePath}/gz/商店光锥推荐.jpg`),
   ]
   e.reply(msg)
   return true
  }
+
 
 }
